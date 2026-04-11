1fce8c80db1e7a00cdd4981dc030552372739e46
MaazUrRehman <82591537+MaazUrRehman@users.noreply.github.com>
2026-04-11 13:12:04 +0500

    fix: moved app to root

---
+++ app/about/page.tsx
@@ -0,0 +1,21 @@
+import AboutHeader from "@/components/about/AboutHeader";
+import CompanyIntro from "@/components/about/CompanyIntro";
+import BusinessValues from "@/components/about/BusinessValues";
+import AboutCTA from "@/components/about/AboutCTA";
+import CoreValues from "@/components/about/CoreValues";
+import OurStory from "@/components/about/OurStory";
+import ExperienceExpertise from "@/components/about/ExperienceExpertise";
+
+export default function AboutPage() {
+  return (
+    <div className="flex flex-col min-h-screen bg-white">
+      <AboutHeader />
+      <CompanyIntro />
+      <BusinessValues />
+      <AboutCTA />
+      <CoreValues />
+      <OurStory />
+      <ExperienceExpertise />
+    </div>
+  );
+}
